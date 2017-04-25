2bbf331140577b832fc751c0af479a89c3c75ad1
DanilMakogon <flanterz@yandex.ru>
2017-04-25 20:59:45 +0700

    Implement check input

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int CheckInput(int time, int sum)
+{
+	if ((time >= 0) && (time <=365) && (sum >= 10000)) {
+		return 1;
+	}
+	return 0;
+}
+
 int main() {
 	int time, sum, percent;
 	printf("Input days:");
